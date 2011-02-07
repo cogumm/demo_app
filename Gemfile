35f5e0c6f608f2612f45db151bc3aa0d777613be
Gabriel F. Vilar (CoGUMm) <cogumm@gmail.com>
2011-02-07 16:04:54 -0300

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.3'
+
+gem 'pg', '0.9.0'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '0810a991a76dcfc830a7c357972d783966a7a13b55f87b8b99d247d92b84e932739b52343a2d1bf83ebdc1565edb4f8c8e567e82970d04a855c8f834bfbe3b79'
