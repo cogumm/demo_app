689dfce9918f87c2d75e1f8d282170c0e9a4707e
Gabriel F. Vilar (CoGUMm) <cogumm@gmail.com>
2011-02-07 16:52:30 -0300

    Finished layout and routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ActiveRecord::Base
+  attr_accessor :name, :email
+
+  def initialize(attributes = {})
+    @name  = attributes[:name]
+    @email = attributes[:email]
+  end
+
+  def formatted_email
+    "#{@name} <#{@email}>"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
 DemoApp::Application.routes.draw do
+  root :to => "pages#home"
+  
   resources :microposts
   resources :users
+
+  get "users/new"
   
   get "pages/home"
   get "pages/contact"
   get "pages/about"
   get "pages/help"
   
-  #root :to => "user#index"
+  match '/signup',  :to => 'users#new'
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+  
+  
 end
