83742133b544b7e71e6a45f2138e03d85d7edadf
Gabriel F. Vilar (CoGUMm) <cogumm@gmail.com>
2011-02-07 21:59:46 -0300

    Done with sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,23 +1,28 @@
 class User < ActiveRecord::Base
   attr_accessor :password
   attr_accessor :name, :email, :password, :password_confirmation
-  
+
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  
+
   validates :name, :presence => true, :length   => { :maximum => 50 }, :uniqueness => { :case_sensitive => true }
   validates :email, :presence => true, :format => { :with => email_regex }, :uniqueness => { :case_sensitive => false }
   validates :password, :presence => true, :confirmation => true, :length => { :within => 6..40 }
-  
+
   before_save :encrypt_password
 
   def has_password?(submitted_password)
     encrypted_password == encrypt(submitted_password)
   end
-  
+
   def self.authenticate(email, submitted_password)
     user = find_by_email(email)
     return nil  if user.nil?
     return user if user.has_password?(submitted_password)
+  end
+
+  def self.authenticate_with_salt(id, cookie_salt)
+    user = find_by_id(id)
+    (user && user.salt == cookie_salt) ? user : nil
   end
 
   private
@@ -38,3 +43,4 @@ class User < ActiveRecord::Base
       Digest::SHA2.hexdigest(string)
     end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,25 @@
 DemoApp::Application.routes.draw do
   root :to => "pages#home"
-  
+
   resources :microposts
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
 
   get "users/new"
-  
+  get "sessions/new"
+
   get "pages/home"
   get "pages/contact"
   get "pages/about"
   get "pages/help"
-  
-  match '/signup',  :to => 'users#new'
+
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
-  
-  
+
+  match '/signup',  :to => 'users#new'
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
+
 end
+
